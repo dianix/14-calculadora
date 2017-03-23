36851af5b23b43836bdf9d5eedb16480c69eed8c
Diana <dianavazquez.dcg@gmail.com>
2017-03-23 16:46:37 -0600

    mis intentos fallidos

---
+++ calculadora-opcionMasElaborada/javascript.js
@@ -0,0 +1,27 @@
+
+function ingresarValores(valor) {
+    var valorRecibido = document.getElementById("resultado");
+    valorRecibido.value += valor;
+}
+
+
+// lo mismo que la funcion de arriba pero de otra manera jajaja
+/*function inputRecibido(elemento) {
+    var valorInicial=document.getElementById('resultado').value;
+    if(valorInicial == '0') {
+        document.getElementById('resultado').value= elemento;
+    }
+    else {
+        var valorNuevo=valor;
+        valorInicial += valorNuevo;
+        document.getElementById('resultado').value=valorInicial;
+    }
+}*/
+
+
+//function operaciones(operador) {
+//    switch (operador){
+//            case '+'
+//    }
+//}
+
